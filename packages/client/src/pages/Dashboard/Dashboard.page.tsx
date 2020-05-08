4622b1ac6afc9e3835235fe2b65000328b029b68
Frankie Law <frankie.sc.law@hotmail.ca>
2020-05-08 15:32:54 -0400

    feat: adding the modal join path

--- packages/client/src/pages/Dashboard/Dashboard.page.tsx
+++ packages/client/src/pages/Dashboard/Dashboard.page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Page } from '../../components/Page/Page';
-import { LeaderboardWidget } from '../../components/LeaderBoardWidget/LeaderBoardWidget';
+// import { LeaderboardWidget } from '../../components/LeaderBoardWidget/LeaderBoardWidget';
+import { JoinPathPage } from '../JoinPath/JoinPath.page';
 
 export const DashboardPage = () => (
   <Page title="Dashboard" type="dashboard" className="bg-white">
-    <h1>Dashboard</h1>
-    <LeaderboardWidget className="w-64" />
+    {/* <h1>Dashboard</h1> */}
+    {/* <LeaderboardWidget className="w-64" /> */}
+    <JoinPathPage />
   </Page>
 );
